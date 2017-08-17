40f870ad440cc937412b6400c4fa2a468b3cbc14
Sean McPherson <seanmcp25@gmail.com>
2017-08-17 15:36:49 -0400

    Add login function without validation; add logout function

--- route/index.js
+++ route/index.js
@@ -1,12 +1,43 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(){
-  req.render('login');
+let users = [{username: 'sean', password: 'pw'}];
+
+router.get('/', function(req, res){ // Works
+  if(req.session.token){
+    res.render('welcome', req.session.user);
+  } else {
+    res.redirect('login');
+  }
 });
 
-router.post('/loginCheck', function(){
-  console.log(req.body);
+router.get('/login', function(req, res){
+  res.render('login');
+});
+
+router.post('/login', function(req, res){
+  let user = {
+    username: req.body.username,
+    password: req.body.password
+  }
+
+  users.forEach(function(aUser){
+    if(aUser.username == user.username && aUser.password == user.password){
+      req.session.token = 'Speak friend and enter'; // Works
+      req.session.user = user; // Works
+      res.redirect('/');
+    } else {
+      res.redirect('/');
+    }
+  });
+
+});
+
+router.get('/logout', function(req, res){ // Works
+  req.session.destroy(function(err){
+    console.log(err);
+  })
+  res.redirect('/');
 });
 
 module.exports = router;
