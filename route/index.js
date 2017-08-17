40c07f0fd6fb55319412f2599f780f6e93d9f4ff
Sean McPherson <seanmcp25@gmail.com>
2017-08-17 16:14:54 -0400

    Basic function complete

--- route/index.js
+++ route/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-let users = [{username: 'sean', password: 'pw'}];
+let users = [{username: 'mcpherson', password: 'mcpherson'}];
 
 router.get('/', function(req, res){ // Works
   if(req.session.token){
@@ -16,26 +16,50 @@ router.get('/login', function(req, res){
 });
 
 router.post('/login', function(req, res){
-  let user = {
-    username: req.body.username,
-    password: req.body.password
-  }
 
-  users.forEach(function(aUser){
-    if(aUser.username == user.username && aUser.password == user.password){
-      req.session.token = 'Speak friend and enter'; // Works
-      req.session.user = user; // Works
-      res.redirect('/');
+  // Begin validation check
+  req.checkBody('username', 'Your username cannot be empty').notEmpty();
+  req.checkBody('username', 'Usernames must be 8 to 20 characters long').len(8, 20);
+  req.checkBody('password', 'Your password cannot be empty').notEmpty();
+  req.checkBody('password', 'Password must be 8 to 20 characters long').len(8, 20);
+
+  let errors = req.getValidationResult();
+  let messages = [];
+
+  errors.then(function(result){
+    result.array().forEach(function(error){
+      messages.push(error.msg);
+    });
+
+    let errArr = {
+      errors: messages
+    };
+    console.log('errArr:', errArr);
+    // End validation check
+    if(messages[0]){
+      console.log('messages is true');
+      res.render('login', {errArr: errArr});
     } else {
-      res.redirect('/');
+      let user = {
+        username: req.body.username,
+        password: req.body.password
+      }
+      users.forEach(function(aUser){
+        if(aUser.username == user.username && aUser.password == user.password){
+          req.session.token = 'Speak friend and enter'; // Works
+          req.session.user = user; // Works
+          res.redirect('/');
+        } else {
+          res.redirect('/');
+        }
+      });
     }
   });
-
 });
 
 router.get('/logout', function(req, res){ // Works
   req.session.destroy(function(err){
-    console.log(err);
+    console.log('Logout error:', err);
   })
   res.redirect('/');
 });
